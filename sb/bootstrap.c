ec433c78a0bccf0ce7c530a96bba8a0b71de4d40
Luikore <Luikore@users.noreply.github.com>
2015-12-20 10:26:28 +0800

    Tweak bootstrap AST

--- sb/bootstrap.c
+++ sb/bootstrap.c
@@ -1,19 +1,14 @@
-#include "spellbreak.h"
-
-#define NODE(context, type, size, ...) ({\
-  nb_node_meta_def_class(ctx->meta, #context, "<"#type, size);\
-  nb_syntax_node_new_v(ctx->arena, #context, #type, size, __VA_ARGS__);\
-})
-
-#define TOKEN(type_name, content) ({\
-  nb_node_meta_def_token(ctx->meta, "."type_name);\
-  nb_token_node_new_c(ctx->arena, type_name, content);\
-})
-
-#define CONS(e, list) nb_cons_node_new(ctx->arena, e, list)
+#include "sb.h"
 
 #define STR(s) nb_string_new_literal_c(s)
 
-Val nb_spellbreak_bootstrap(Ctx* ctx) {
+// XXX sizeof((Val[]){__VA_ARGS__}) can explode clang's memory
+#define NODE(type, argc, ...) nb_struct_new(klass_find(STR(#type), namespace), argc, (Val[]){__VA_ARGS__})
+
+#define TOKEN(type_name, content) nb_token_new_c(STR(type_name), content)
+
+#define LIST(...) nb_cons_list(sizeof((Val[]){__VA_ARGS__}) / sizeof(Val), (Val[]){__VA_ARGS__})
+
+Val nb_spellbreak_bootstrap(Ctx* ctx, uint32_t namespace) {
 # include "bootstrap.inc"
 }
