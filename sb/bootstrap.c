39cb7c49fcf52809401cfc8a589eef9e6cb0782e
Luikore <Luikore@users.noreply.github.com>
2016-02-10 22:36:23 +0800

    WIP regexp compiling

--- sb/bootstrap.c
+++ sb/bootstrap.c
@@ -1,9 +1,7 @@
 #include "sb.h"
 
-#define STR(s) nb_string_new_literal_c(s)
-
-static uint32_t FIND_K(const char* klass, uint32_t parent) {
-  uint32_t k = klass_find(STR(klass), parent);
+static uint32_t _guarded_klass_find_c(const char* name, uint32_t namespace) {
+  uint32_t k = klass_find_c(name, namespace);
   if (k) {
     return k;
   } else {
@@ -16,8 +14,10 @@ static uint32_t FIND_K(const char* klass, uint32_t parent) {
   }
 }
 
+#define STR(s) nb_string_new_literal_c(s)
+
 // XXX sizeof((Val[]){__VA_ARGS__}) can explode clang's memory
-#define NODE(type, argc, ...) nb_struct_anew(arena, FIND_K(#type, namespace), argc, (Val[]){__VA_ARGS__})
+#define NODE(type, argc, ...) nb_struct_anew(arena, _guarded_klass_find_c(#type, namespace), argc, (Val[]){__VA_ARGS__})
 
 // #define TOKEN(type_name, content, val) nb_token_new_c(STR(type_name), content, val)
 
--- sb/vm-regexp.c
+++ sb/vm-regexp.c
@@ -42,16 +42,15 @@ typedef struct {
   }\
 } while (0)
 
+#pragma mark ### exec
+
 typedef struct {
   uint16_t* pc;
   int32_t saved[20];
 } Thread;
 
 MUT_ARRAY_DECL(Threads, Thread);
-
-Val sb_vm_regexp_compile(struct Iseq* iseq, void* arena, Val patterns_dict, Val node) {
-  return VAL_NIL;
-}
+MUT_ARRAY_DECL(Stack, Val);
 
 #define CAPTURE_CAP sizeof(int32_t) * 20
 
@@ -166,6 +165,8 @@ not_match:
   return false;
 }
 
+#pragma mark ### compile
+
 Val sb_vm_regexp_from_string(struct Iseq* iseq, Val s) {
   if (VAL_KLASS(s) != KLASS_STRING) {
     return nb_string_new_literal_c("not string");
@@ -180,5 +181,193 @@ Val sb_vm_regexp_from_string(struct Iseq* iseq, Val s) {
   ENCODE(iseq, uint16_t, MATCH);
   ENCODE(iseq, uint16_t, END);
 
+  return VAL_NIL;
+}
+
+// labels[num] = pos
+MUT_ARRAY_DECL(Labels, int);
+
+// forks = [iseq_pos1, iseq_pos2, ...]
+MUT_ARRAY_DECL(Forks, int);
+
+// jmps = [iseq_pos1, iseq_pos2, ...]
+MUT_ARRAY_DECL(Jmps, int);
+
+static Val MATCH_NODE=0, LABEL_NODE=0, JMP_NODE=0, FORK_NODE=0;
+
+static void _ensure_tags() {
+    if (!MATCH_NODE) {
+    void* ptr  = malloc(sizeof(Val) * 4);
+    MATCH_NODE = (Val)ptr;
+    LABEL_NODE = (Val)(ptr + 1);
+    JMP_NODE   = (Val)(ptr + 2);
+    FORK_NODE  = (Val)(ptr + 3);
+  }
+}
+
+static int _new_label(Labels* labels) {
+  int i = Labels.size(labels);
+  Labels.push(labels, 0);
+  return i;
+}
+
+static void _push_label(struct Stack* stack, Val label) {
+  Stack.push(stack, label);
+  Stack.push(stack, LABEL_NODE);
+}
+
+static void _push_fork(struct Stack* stack, Val label1, Val label2) {
+  Stack.push(stack, label2);
+  Stack.push(stack, label1);
+  Stack.push(stack, FORK_NODE);
+}
+
+static void _push_seq(struct Stack* stack, Val seq) {
+  Val content = nb_struct_get(seq, 0);
+  for (Val tail = content; tail != VAL_NIL; tail = nb_cons_tail(tail)) {
+    Stack.push(stack, nb_cons_head(tail));
+  }
+}
+
+static void _push_branches(struct Stack* stack, Val branches) {
+  /* example encoding e1 | e2 | e3
+
+    fork L1 L2
+    L1: e1
+    jmp L0
+    L2:
+
+    fork L3 L4
+    L3: e2
+    jmp L0
+    L4
+
+    fork L5 L6
+    L5: e3
+    jmp L0
+    L6:
+
+    L0:
+
+  */
+  Val label0 = _new_label(&labels);
+  _push_label(&stack, label0);
+  for (Val tail = branches; tail != VAL_NIL; tail = nb_cons_tail(tail)) {
+
+    Val label1 = _new_label(&labels);
+    Val label2 = _new_label(&labels);
+
+    _push_label(&stack, label2);
+    _push_jmp(&stack, label0);
+    Stack.push(&stack, nb_cons_head(tail));
+    _push_label(&stack, label1);
+
+    _push_fork(&stack, label1, label2);
+  }
+}
+
+static void _encode_range(struct Iseq* iseq, Val range_node) {
+}
+
+Val sb_vm_regexp_compile(struct Iseq* iseq, void* arena, Val patterns_dict, Val node) {
+  _ensure_tags();
+
+  /*
+  compile stack layout
+  - nodes to process are pushed from back to forth.
+    since list nodes are already constructed in reverse order,
+    so no need to reverse before pushing.
+  - special nodes are pushed by compiler, and contents are inlined:
+    - match: [MATCH_NODE]
+    - label: [label_num, LABEL_NODE]
+    - jmp:   [label_num, JMP_NODE]
+    - fork:  [label_num2, label_num1, FORK_NODE]
+
+  encoding of fork, jmp & labels
+  1. create label nodes, only number is stored.
+  2. generate label node: fill curr pos in Labels.
+     generate fork/jmp node: encode label numbers instead.
+  3. go through Splits and Jmps, replace all label numbers with label pos.
+  */
+
+  uint32_t kSeq              = klass_find_c("Seq", sb_klass());
+  uint32_t kPredefAnchor     = klass_find_c("PredefAnchor", sb_klass());
+  uint32_t kFlag             = klass_find_c("Flag", sb_klass());
+  uint32_t kQuantified       = klass_find_c("Quantified", sb_klass());
+  uint32_t kQuantifiedRange  = klass_find_c("QuantifiedRange", sb_klass());
+  uint32_t kUnit             = klass_find_c("Unit", sb_klass());
+  uint32_t kGroup            = klass_find_c("Group", sb_klass());
+  uint32_t kCharGroupPredef  = klass_find_c("CharGroupPredef", sb_klass());
+  uint32_t kUnicodeCharClass = klass_find_c("UnicodeCharClass", sb_klass());
+  uint32_t kBracketCharGroup = klass_find_c("BracketCharGroup", sb_klass());
+  uint32_t kCharRange        = klass_find_c("CharRange", sb_klass());
+
+  struct Stack stack;
+  struct Labels labels;
+  struct Forks forks;
+  Stack.init(&stack);
+  Labels.init(&labels);
+  Forks.init(&forks);
+
+  Stack.push(&stack, MATCH_NODE);
+
+  // loop compile
+  Val content = nb_struct_get(node, 0);
+  Stack.push(&stack, content);
+  while (Stack.size(&stack)) {
+    Val curr = Stack.pop(&stack);
+    if (curr == LABEL_NODE) {
+      continue;
+    } else if (curr == FORK_NODE) {
+      continue;
+    } else if (curr == JMP_NODE) {
+      continue;
+    } else if (curr == MATCH_NODE) {
+      continue;
+    }
+
+    uint32_t klass = VAL_KLASS(curr);
+    if (klass == KLASS_INTEGER) { // char
+      int chr = VAL_TO_INT(curr);
+      ENCODE(iseq, Arg32, ((Arg32){CHAR, chr}));
+
+    } else if (klass == kCharRange) {
+      _encode_range(&stack, curr);
+    } else if (klass == kSeq) {
+      _push_seq(&stack, curr);
+    } else if (klass == KLASS_CONS) { // branches
+      _push_branches(&stack, curr);
+    } else if (klass == kPredefAnchor) {
+    } else if (klass == kFlag) {
+    } else if (klass == kQuantified) {
+    } else if (klass == kQuantifiedRange) {
+    } else if (klass == kUnit) {
+    } else if (klass == kGroup) {
+    } else if (klass == kCharGroupPredef) {
+    } else if (klass == kUnicodeCharClass) {
+    } else if (klass == kBracketCharGroup) {
+    } else {
+      val_throw(nb_string_new_literal_c("unrecognized klass"));
+    }
+  }
+  // Seq[$1]
+  // PredefAnchor[$1]
+  // Flag[$1]
+  // Quantified[$1, $2]
+  // QuantifiedRange[$1, $3, $4, $6]
+  // Unit[$1]
+  // Group[$2, $3]
+  // CharGroupPredef[$1]
+  // UnicodeCharClass[$1]
+  // BracketCharGroup[$1, $2]
+  // CharRange[$1, $3]
+
+  _compute_labels(iseq, &labels, &forks);
+
+  Stack.cleanup(&stack);
+  Labels.cleanup(&labels);
+  Forks.cleanup(&forks);
+
+  ENCODE(iseq, uint16_t, END);
   return VAL_NIL;
 }
